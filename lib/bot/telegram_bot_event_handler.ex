d1800394e21fbcb9d8b996bd8724efd4c25e60c8
Levente <leventea@tuta.io>
2022-04-30 22:52:56 +0200

    Randomly respond to messages

--- lib/bot/telegram_bot_event_handler.ex
+++ lib/bot/telegram_bot_event_handler.ex
@@ -23,5 +23,12 @@ defmodule TelegramBot.EventHandler do
 
   def handle_message(msg) do
     SentenceProvider.train(SentenceProvider, [ msg.text ])
+
+    # randomly reply to the message
+    # NOTE: should probably use config files for this
+    if Enum.random(1..100) < 25 do
+      {:ok, response} = SentenceProvider.generate_sentence(SentenceProvider)
+      reply(msg, response)
+    end
   end
 end
