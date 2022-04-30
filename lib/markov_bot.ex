de38241a2f8165f411d565768b266ff29ca747da
Levente <leventea@tuta.io>
2022-04-30 22:34:26 +0200

    Automatically load chain state

--- lib/markov_bot.ex
+++ lib/markov_bot.ex
@@ -2,6 +2,14 @@ defmodule MarkovBot do
   use Application
 
   def start(_type, _args) do
-    MarkovBot.Supervisor.start_link()
+    {:ok, main_sup} = MarkovBot.Supervisor.start_link()
+
+    # load the previous chain state auto-save
+    chain_path = Path.absname("./state.chain")
+    if File.exists?(chain_path) do
+      SentenceProvider.load_state(SentenceProvider, chain_path)
+    end
+
+    {:ok, main_sup}
   end
 end
