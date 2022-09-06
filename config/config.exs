5fbdca982c8ce8db637fd5aeb3529c30cca7e9d7
Levente <leventea@tuta.io>
2022-09-06 16:02:32 +0200

    Delete config values
    I already revoked the bot token, whoops

--- config/config.exs
+++ config/config.exs
@@ -4,7 +4,7 @@ alias MarkovBot.Scheduler.Tasks
 
 config :nadia,
   # string, the bot's token, can be obtained from @BotFather
-  token: "5109198901:AAEMNF94AqN5YEbe65kKL6trH3rnxsQySV4"
+  token: ""
 
 config :markov_bot,
   log_ignored: false,                 # boolean, if set to true, it will log info related to ignored messages
@@ -12,8 +12,8 @@ config :markov_bot,
   backup_dir: "./backups",            # string, directory the backups get copied to
   autosave_path: "./state.chain",     # string, path the chain gets auto-saved to
   keep_backups_for: 7,                # integer, discard backups older than n days
-  admins: [ 532217020, 1741227329 ],  # list of integers, people allowed to run admin-only commands
-  chat_ids: [ -1001382033469, -1001415737136 ], # list of integers, the only chat(s) the bot trains on, responds to
+  admins: [ ],                        # list of integers, people allowed to run admin-only commands
+  chat_ids: [ ],                      # list of integers, the only chat(s) the bot trains on, responds to
   do_random_reply: fn ->              # function, must return bool, if it returns true the bot will reply to a message
     Enum.random(1..100) < 5
   end
